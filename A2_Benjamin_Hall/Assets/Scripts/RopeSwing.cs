27a37d6b0225016acef6ca21d3d0f8ac63cd662e
Benjamin Hall <BHall2@myjmc.edu.au>
2018-11-06 14:19:58 +1100

    Raycaster

---
+++ A2_Benjamin_Hall/Assets/Scripts/RopeSwing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSwing : MonoBehaviour {
+
+    public float tetherDistance;
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    void Update()
+   {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+    // This would cast rays only against colliders in layer 8.
+    // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+    layerMask = ~layerMask;
+
+    RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, tetherDistance, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.green);
+            hit.collider.gameObject.GetComponent<Renderer>().material.color = Color.green;
+    Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
