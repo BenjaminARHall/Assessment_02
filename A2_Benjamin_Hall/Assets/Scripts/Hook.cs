81660df4e2893b1d8091560e4a76da975d42c171
Benjamin Hall <BHall2@myjmc.edu.au>
2018-11-13 15:39:11 +1100

    Raycast grapple
    Going back to square one using a raycast grapple system and then building up to hinges.

---
+++ A2_Benjamin_Hall/Assets/Scripts/Hook.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hook : MonoBehaviour {
+
+    public Transform cam;
+
+    private RaycastHit hit;
+
+    private Rigidbody rb;
+
+    public bool attached = false;
+
+    public float momentum;
+    public float speed;
+    public float step;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast (cam.position, cam.forward, out hit))
+            {
+            attached = true;
+                rb.isKinematic = true;
+                
+            }
+           
+
+        }
+
+        if (attached)
+        {
+            momentum += Time.deltaTime * speed;
+            step = momentum * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, hit.point, step);
+        }
+	}
+}
