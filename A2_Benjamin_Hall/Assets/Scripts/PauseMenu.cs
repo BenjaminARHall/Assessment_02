ea22b489bceeca8d52ba9c9be1eb1d25748228ca
BenjaminARHall <b-a-hall@hotmail.com>
2018-12-23 23:21:50 +1100

    Added assets
    Added assets

---
+++ A2_Benjamin_Hall/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public GameObject pausePanel;
+    public bool isPaused = false;
+
+    public void Start()
+    {
+        isPaused = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            TogglePauseMenu();
+    }
+
+    public void TogglePauseMenu()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused == true)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+}
+   
